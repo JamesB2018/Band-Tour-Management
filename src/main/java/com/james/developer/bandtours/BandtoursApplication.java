8e7ba4d3def974536cb25c9674fe31d18beb42a2
James <jmb47580@ucmo.edu>
2019-03-03 18:38:26 -0600

    My First Commit

---
+++ src/main/java/com/james/developer/bandtours/BandtoursApplication.java
@@ -0,0 +1,13 @@
+package com.james.developer.bandtours;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BandtoursApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BandtoursApplication.class, args);
+    }
+
+}
