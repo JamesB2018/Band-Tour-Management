17b5b15245f9ec614f1778e9b5d18242114d7199
James <jmb47580@ucmo.edu>
2019-03-04 19:54:11 -0600

    Created a repository to manage the group entity

---
+++ src/main/java/com/james/developer/bandtours/model/User.java
@@ -0,0 +1,21 @@
+package com.james.developer.bandtours.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    private String id;
+    private String name;
+    private String email;
+}
